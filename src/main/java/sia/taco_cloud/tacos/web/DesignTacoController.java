7fb5c4ce9e4f75458753475d1d8133aef87b236d
matthieu <mcaron123@gmail.com>
2025-04-03 10:28:00 +0200

    fin chapitre 2

--- src/main/java/sia/taco_cloud/tacos/web/DesignTacoController.java
+++ src/main/java/sia/taco_cloud/tacos/web/DesignTacoController.java
@@ -3,8 +3,11 @@ package sia.taco_cloud.tacos.web;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -67,8 +70,12 @@ public class DesignTacoController {
     }
 
     @PostMapping
-    public String processTaco(Taco taco,
+    public String processTaco(@Valid Taco taco, Errors errors,
                               @ModelAttribute TacoOrder tacoOrder) {
+        if (errors.hasErrors()) {
+            return "design";
+        }
+
         tacoOrder.addTaco(taco);
         log.info("Processing taco: {}", taco);
         return "redirect:/orders/current";
