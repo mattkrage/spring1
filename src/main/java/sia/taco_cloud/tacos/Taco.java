37ffcc316877d6d72be63d3ba93cbdb62ecb1027
matthieu <mcaron123@gmail.com>
2025-04-09 14:51:42 +0200

    first REST ato config (data-api path)

--- src/main/java/sia/taco_cloud/tacos/Taco.java
+++ src/main/java/sia/taco_cloud/tacos/Taco.java
@@ -12,9 +12,11 @@ import lombok.Data;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import jakarta.persistence.Id;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 @Data
 @Entity
+@RestResource(rel="tacos", path="tacos")
 public class Taco {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
